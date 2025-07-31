2f76c8034bb0a01317c1614a342951d76c7ed93e
Algorithmic Simplicity <u5826185@anu.edu.au>
2025-07-31 15:14:46 +1000

    Added camera helper functions set_to_orthographic and set_distance_to_screen to change camera perspective.

--- algan/rendering/camera.py
+++ algan/rendering/camera.py
@@ -58,12 +58,12 @@ class Camera(Mob):
             # self.animatable_attrs.update({'light_source_location'})
             self.spawn(animate=False)
 
-    #def set_screen_distance(self, distance):
-    #    old_dist = dot_product(self.screen.location, self.get_forward_direction())
-    #    with Sync():
-    #        self.screen.move_to(self.location + self.get_forward_direction() * distance)
-    #        self.screen.scale(distance / old_dist)
-    #    return self
+    def set_to_orthographic(self):
+        return self.set_distance_to_screen(1e5)
+
+    def set_distance_to_screen(self, distance):
+        self.set_non_recursive(location=self.screen.location - self.get_forward_direction() * distance)
+        return self
 
     def set_euler_angles(self, angle_1, angle_2, angle_3):
         with Sync():
